1d500cc348e21a2b8185c795fe97923e85176aa7
Gareth John <gareth.e.john@pwc.com>
2021-11-03 14:11:24 +0000

    Added Router, About page and Navbar links to the home and about pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About This React App</h1>
+            <p>This is an app to search for, and display GitHub user profiles</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About
+
