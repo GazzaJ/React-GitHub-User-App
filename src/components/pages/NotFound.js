3780defc2a987d4ef79e726e7baf5db4c2bd69ea
Gareth John <gareth.e.john@pwc.com>
2021-11-05 18:57:19 +0000

    Add Home and Not Found components

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Oops something went wrong!</h1>
+            <p className="lead">The github page you are looking for does not exist...</p>
+        </div>
+    )
+}
+
+export default NotFound
